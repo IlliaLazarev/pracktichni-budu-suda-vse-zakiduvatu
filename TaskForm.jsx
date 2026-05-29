8c108aef3282c84200d83c1f79440c0a7be0e70f
IlliaLazarev <lazarevillia2008@gmail.com>
2026-05-29 17:53:09 +0300

    Add TaskForm component for task input

---
+++ TaskForm.jsx
@@ -0,0 +1,27 @@
+import { useContext, useRef } from "react";
+import TaskContext from "../context/TaskContext";
+
+function TaskForm() {
+  const { addTask } = useContext(TaskContext);
+  const inputRef = useRef();
+
+  function handleAddTask() {
+    const title = inputRef.current.value;
+
+    if (title.trim() === "") return;
+
+    addTask(title);
+
+    inputRef.current.value = "";
+    inputRef.current.focus();
+  }
+
+  return (
+    <div>
+      <input ref={inputRef} type="text" placeholder="Введіть задачу" />
+      <button onClick={handleAddTask}>Додати</button>
+    </div>
+  );
+}
+
+export default TaskForm;
